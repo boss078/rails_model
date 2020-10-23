d3436d3a66492ad852e10ccdb8e5ced430844dc4
boss078 <36530564+boss078@users.noreply.github.com>
2020-10-23 16:33:38 +0300

    fix for videogame

--- app/models/videogame.rb
+++ app/models/videogame.rb
@@ -2,5 +2,5 @@ class Videogame < ApplicationRecord
     validates :title, presence: true
     validates :platform, presence: true
     validates :description, length: { maximum: 300 }
-    validates_date :release_date, :before => :today
+    validates :release_date, presence: true
 end
